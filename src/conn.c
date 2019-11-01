ab15fa1c25659995fcd2d8e1c3b40394794de042
Erik Rigtorp <erik@rigtorp.se>
2019-11-01 09:04:00 -0700

    Always reconnect on errors

--- src/conn.c
+++ src/conn.c
@@ -2661,11 +2661,7 @@ natsConn_processErr(natsConnection *nc, char *buf, int bufLen)
     }
     else
     {
-        close = true;
-        natsConn_Lock(nc);
-        nc->err = NATS_ERR;
-        snprintf(nc->errStr, sizeof(nc->errStr), "%s", error);
-        natsConn_Unlock(nc);
+        _processOpError(nc, NATS_ERR, false);
     }
     if (close)
         _close(nc, NATS_CONN_STATUS_CLOSED, false, true);

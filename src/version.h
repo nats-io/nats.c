4292c005ff287d02217cb7188ebdef7e8cde018e
Ivan Kozlovic <ivan@synadia.com>
2023-03-07 13:33:17 -0700

    Bump release to v3.6.2-beta as per release process
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- src/version.h
+++ src/version.h
@@ -23,9 +23,9 @@ extern "C" {
 
 #define NATS_VERSION_MAJOR  3
 #define NATS_VERSION_MINOR  6
-#define NATS_VERSION_PATCH  1
+#define NATS_VERSION_PATCH  2
 
-#define NATS_VERSION_STRING "3.6.1"
+#define NATS_VERSION_STRING "3.6.2-beta"
 			 				  
 #define NATS_VERSION_NUMBER ((NATS_VERSION_MAJOR << 16) | \
                              (NATS_VERSION_MINOR <<  8) | \

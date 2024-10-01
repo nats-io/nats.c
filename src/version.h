3c1c94963f49dce6530ff27007784561e2aff28b
Lev <1187448+levb@users.noreply.github.com>
2024-10-01 13:02:20 -0700

    Bumped next release version to v3.10.0-beta (#807)

--- src/version.h
+++ src/version.h
@@ -22,10 +22,10 @@ extern "C" {
 #endif
 
 #define NATS_VERSION_MAJOR  3
-#define NATS_VERSION_MINOR  9
+#define NATS_VERSION_MINOR  10
 #define NATS_VERSION_PATCH  0
 
-#define NATS_VERSION_STRING "3.9.0-beta"
+#define NATS_VERSION_STRING "3.10.0-beta"
 			 				  
 #define NATS_VERSION_NUMBER ((NATS_VERSION_MAJOR << 16) | \
                              (NATS_VERSION_MINOR <<  8) | \

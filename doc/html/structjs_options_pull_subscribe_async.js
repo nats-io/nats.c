96f3474b804a0b33722133b08fe799e2f4cd7abc
levb <1187448+levb@users.noreply.github.com>
2025-02-18 15:54:55 +0000

    [skip ci] Update docs: 07df538: fix: typo in clear routine (#842)

---
+++ doc/html/structjs_options_pull_subscribe_async.js
@@ -0,0 +1,14 @@
+var structjs_options_pull_subscribe_async =
+[
+    [ "Timeout", "structjs_options_pull_subscribe_async.html#a3dcad3cffbee77d14ceaa392dfa377c5", null ],
+    [ "MaxMessages", "structjs_options_pull_subscribe_async.html#a1f7da5b5cd38f3a8ebb729d4a1ffd0f8", null ],
+    [ "MaxBytes", "structjs_options_pull_subscribe_async.html#a1355dfc02b1e66bb6d0edba751265e24", null ],
+    [ "NoWait", "structjs_options_pull_subscribe_async.html#a858afc43e24572377214d072d5bff318", null ],
+    [ "CompleteHandler", "structjs_options_pull_subscribe_async.html#a6224bf50d314a83ed2df2c84a6966055", null ],
+    [ "CompleteHandlerClosure", "structjs_options_pull_subscribe_async.html#aa5437f766db36d61595cc0471b2b65c2", null ],
+    [ "Heartbeat", "structjs_options_pull_subscribe_async.html#a6583dfdee172d82b6877d9e6ff4e845c", null ],
+    [ "FetchSize", "structjs_options_pull_subscribe_async.html#a1bf6fa8ddd19698bd82f4c27ddb06a9f", null ],
+    [ "KeepAhead", "structjs_options_pull_subscribe_async.html#ae50d78c4012666aca20b7a01341d8d07", null ],
+    [ "NextHandler", "structjs_options_pull_subscribe_async.html#a591cac37de66ace6a96f7c73fc305661", null ],
+    [ "NextHandlerClosure", "structjs_options_pull_subscribe_async.html#a1c3835372d48eeec930ce7203f058791", null ]
+];

6f9b421efecbcff5ff75ad90b201f99e7ac91bc6
Burkov Egor <eburkov@rvision.ru>
2025-02-18 16:20:21 +0300

    fix: some nulls checks - better if for sub.c

--- src/sub.c
+++ src/sub.c
@@ -656,12 +656,15 @@ natsSub_nextMsg(natsMsg **nextMsg, natsSubscription *sub, int64_t timeout, bool 
     if (s == NATS_OK)
     {
         msg = sub->ownDispatcher.queue.head;
-        if ((msg == NULL) && sub->draining)
+        if (msg == NULL)
         {
-            removeSub = true;
-            s = NATS_TIMEOUT;
+            if (sub->draining)
+            {
+                removeSub = true;
+                s = NATS_TIMEOUT;
+            }
         }
-        else if (msg != NULL)
+        else
         {
             sub->ownDispatcher.queue.head = msg->next;
 

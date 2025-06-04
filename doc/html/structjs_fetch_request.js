5bc629f806f10ccb693f43c4f84acd61eacb8f5c
kozlovic <13736691+kozlovic@users.noreply.github.com>
2025-06-04 17:04:59 +0000

    [skip ci] Update docs: cc2edc6: Merge pull request #873 from habbbe/fix_null_header_value

--- doc/html/structjs_fetch_request.js
+++ doc/html/structjs_fetch_request.js
@@ -4,5 +4,9 @@ var structjs_fetch_request =
     [ "Batch", "structjs_fetch_request.html#a84f06b60e25decb2674996f0cb315f63", null ],
     [ "MaxBytes", "structjs_fetch_request.html#a3298db691c2ecb9725cc029e2c35d6d1", null ],
     [ "NoWait", "structjs_fetch_request.html#ae251eebb1998a39fe3b898302b58293d", null ],
-    [ "Heartbeat", "structjs_fetch_request.html#ae8e73c7cc2e2a2d2502407c9b1522496", null ]
+    [ "Heartbeat", "structjs_fetch_request.html#ae8e73c7cc2e2a2d2502407c9b1522496", null ],
+    [ "MinPending", "structjs_fetch_request.html#a5a83f41e8e44bc592a4e6e1a34127e68", null ],
+    [ "MinAckPending", "structjs_fetch_request.html#ab9ef4230eca62471d84bd907e6aec8c9", null ],
+    [ "ID", "structjs_fetch_request.html#a14214f0e033e75b3ee0f8de634f5a553", null ],
+    [ "Group", "structjs_fetch_request.html#a298e98d26850e1105833689c280e4f5e", null ]
 ];

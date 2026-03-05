ee253a7946a65e38f4796423e9d725772cb57ea6
AdamPayzant <46936814+AdamPayzant@users.noreply.github.com>
2026-03-05 18:28:56 +0000

    [skip ci] Update docs: bb497a0: Merge pull request #975 from nats-io/add-cc-reviews

--- doc/html/structjs_pub_options.js
+++ doc/html/structjs_pub_options.js
@@ -6,6 +6,7 @@ var structjs_pub_options =
     [ "ExpectLastMsgId", "structjs_pub_options.html#a095391b33a261586fc02eaf013b65e61", null ],
     [ "ExpectLastSeq", "structjs_pub_options.html#a3b02b60b05caa6d242079a4c26b181bf", null ],
     [ "ExpectLastSubjectSeq", "structjs_pub_options.html#aeefec1d2c7644562d23b87e55bf2e253", null ],
+    [ "ExpectLastSubjectSeqSubject", "structjs_pub_options.html#a932874ab9567150ee4e73fabd6e12de5", null ],
     [ "ExpectNoMessage", "structjs_pub_options.html#a740a7edee8c832a75b75fded6046ba90", null ],
     [ "MsgTTL", "structjs_pub_options.html#a07c8ec214966ef7ce0bd48a8e8008f9d", null ]
 ];

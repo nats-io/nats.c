ab08c8b0007a0b2f6ae6ea8eea63e685abfe6d3b
kozlovic <13736691+kozlovic@users.noreply.github.com>
2026-03-27 15:44:01 +0000

    [skip ci] Update docs: dffb248: Merge pull request #977 from nats-io/atomic_batch_publish

--- doc/html/search/variables_0.js
+++ doc/html/search/variables_0.js
@@ -6,12 +6,13 @@ var searchData=
   ['ackpolicy_3',['AckPolicy',['../structjs_consumer_config.html#a8a311af614dfae1b0f4e4af7db467aab',1,'jsConsumerConfig']]],
   ['ackwait_4',['AckWait',['../structjs_consumer_config.html#a651a94b9b78668dde830dc7869610e67',1,'jsConsumerConfig']]],
   ['active_5',['active',['../structjs_peer_info.html#a8441d023932c388953197f7ea39402fe',1,'jsPeerInfo::Active'],['../structjs_stream_source_info.html#a28b8cc0a9d487672955d3a3fa03de247',1,'jsStreamSourceInfo::Active']]],
-  ['allowdirect_6',['AllowDirect',['../structjs_stream_config.html#a28630f6f07ca02553c5d5718d7c29721',1,'jsStreamConfig']]],
-  ['allowmsgttl_7',['AllowMsgTTL',['../structjs_stream_config.html#a54d080160cd8179c8c6f3982f0b11ab5',1,'jsStreamConfig']]],
-  ['allowrollup_8',['AllowRollup',['../structjs_stream_config.html#aba71cdb342181f52bb9f7ff5f770a1fa',1,'jsStreamConfig']]],
-  ['alternates_9',['Alternates',['../structjs_stream_info.html#a720c4ff9090bafda50000843810ab1bb',1,'jsStreamInfo']]],
-  ['alternateslen_10',['AlternatesLen',['../structjs_stream_info.html#ad37a5fd700bd5df54e5002c7de51c3ad',1,'jsStreamInfo']]],
-  ['api_11',['API',['../structjs_account_info.html#a94ab71c69b7883c2731ae8741ddc1e94',1,'jsAccountInfo']]],
-  ['apiprefix_12',['APIPrefix',['../structjs_external_stream.html#a2eaf2c0c3e77c269f44e254af8b24e96',1,'jsExternalStream']]],
-  ['averageprocessingtimenanoseconds_13',['AverageProcessingTimeNanoseconds',['../structmicro__endpoint__stats__s.html#acc74c3df96d5f8595521a57dd0eb0cde',1,'micro_endpoint_stats_s']]]
+  ['allowatomic_6',['AllowAtomic',['../structjs_stream_config.html#ab0f62e9eff46d8c5150e191af91e31e5',1,'jsStreamConfig']]],
+  ['allowdirect_7',['AllowDirect',['../structjs_stream_config.html#a28630f6f07ca02553c5d5718d7c29721',1,'jsStreamConfig']]],
+  ['allowmsgttl_8',['AllowMsgTTL',['../structjs_stream_config.html#a54d080160cd8179c8c6f3982f0b11ab5',1,'jsStreamConfig']]],
+  ['allowrollup_9',['AllowRollup',['../structjs_stream_config.html#aba71cdb342181f52bb9f7ff5f770a1fa',1,'jsStreamConfig']]],
+  ['alternates_10',['Alternates',['../structjs_stream_info.html#a720c4ff9090bafda50000843810ab1bb',1,'jsStreamInfo']]],
+  ['alternateslen_11',['AlternatesLen',['../structjs_stream_info.html#ad37a5fd700bd5df54e5002c7de51c3ad',1,'jsStreamInfo']]],
+  ['api_12',['API',['../structjs_account_info.html#a94ab71c69b7883c2731ae8741ddc1e94',1,'jsAccountInfo']]],
+  ['apiprefix_13',['APIPrefix',['../structjs_external_stream.html#a2eaf2c0c3e77c269f44e254af8b24e96',1,'jsExternalStream']]],
+  ['averageprocessingtimenanoseconds_14',['AverageProcessingTimeNanoseconds',['../structmicro__endpoint__stats__s.html#acc74c3df96d5f8595521a57dd0eb0cde',1,'micro_endpoint_stats_s']]]
 ];
